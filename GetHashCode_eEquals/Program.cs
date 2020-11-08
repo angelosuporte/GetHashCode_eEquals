a2dd35a518e62449cc9605fb9f89e15f7d59bc80
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-08 14:57:29 -0300

    Adicionar arquivos de projeto.

---
+++ GetHashCode_eEquals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetHashCode_eEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
